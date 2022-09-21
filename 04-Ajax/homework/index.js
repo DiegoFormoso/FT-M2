cf8e084d57e678b9dc311b942646012c3781ea4f
DiegoFormoso <dformoso@yahoo.com>
2022-09-21 20:49:12 -0300

    hw 4 ultima

--- 04-Ajax/homework/index.js
+++ 04-Ajax/homework/index.js
@@ -4,32 +4,28 @@
 $('#boton').click(function(){
     $.get('http://localhost:5000/amigos', function(data) {            
         let lista = document.getElementById('lista');
+        //lista.empty();
         while (lista.firstChild) {
           lista.removeChild(lista.firstChild);      
         }
         data.forEach(element => $(
-             `<li id=${element.id}>
-                <p>${element.id}</p>
-                <p>${element.name}</p>
-                <p>${element.age}</p>
-                <p>${element.email}</p>
-            </li>`).appendTo('#lista'));
+             `<li id=${element.id}>${element.name}</li>`).appendTo('#lista'));
         })
     });
     
 
-$('#search').click(function(){
+$('#search').click(()=>{
     let amigo = document.getElementById('amigo');
     while (amigo.firstChild)
         amigo.removeChild(amigo.firstChild);
 
-    let input = $('#input');
+    let input = $('#input'); //$('#input').val() = input[0].value 
     if (input[0].value === '') {
         $('<p>ingrese un valor !!!</p>').appendTo('#amigo');
     }
     else {
         $.get(`http://localhost:5000/amigos/${input[0].value}`, function(data){
-            $(`<p>${data.id}</p><p>${data.name}</p><p>${data.age}</p><p>${data.email}</p>`).appendTo('#amigo'); 
+            $(`<p>${data.name}</p>`).appendTo('#amigo');  // $('#amigo').text(data.name);
         })
         .fail(function() {
             $(`<p>no encontrado</p>`).appendTo('#amigo');
@@ -50,7 +46,6 @@ $('#delete').click(function(){
             type: 'DELETE',
             url: `http://localhost:5000/amigos/${input[0].value}`,
             success: function(data) {
-                console.log(data);
                 $('<p>delete success</p>').appendTo('#success');
             },
         });        

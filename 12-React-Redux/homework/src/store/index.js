bc82dde6f4d01cbd8415115a99dd3cd0e29c2c86
DiegoFormoso <dformoso@yahoo.com>
2022-10-17 23:08:56 -0300

    hw 12-a

--- 12-React-Redux/homework/src/store/index.js
+++ 12-React-Redux/homework/src/store/index.js
@@ -2,9 +2,16 @@ import { createStore, applyMiddleware, compose } from "redux";
 import rootReducer from "../reducers/index";
 import thunk from "redux-thunk";
 
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(
     rootReducer,
-    applyMiddleware(thunk)
+    composeEnhancer(applyMiddleware(thunk))
 );
+
+
+// const store = createStore(
+//     rootReducer,
+//     applyMiddleware(thunk)
+// );
 
 export default store;
